b147fe96fe0a01d547634c9bc28dbb082ce2b781
Thomas Böhringer <dev@tboehringer.de>
2024-07-15 13:22:18 +0200

    npm upgrades and update tailwind config
    - "next": "^14.2.5",
    - "@types/node": "^20.14.10",
    - "eslint-plugin-jsx-a11y": "^6.9.0",
    - "eslint-plugin-react": "^7.34.4",
    - "postcss": "^8.4.39",
    - "tailwindcss": "^3.4.4",
    - "typescript": "^5.5.3",
    - "typescript-eslint": "^7.16.0"
    - "@heroicons/react": "^2.1.5"
    - "@eslint/js": "^9.7.0"
    - "eslint": "^9.7.0"
    - "eslint-config-next": "^14.2.5"
    - "globals": "^15.8.0"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = withMT({
+module.exports = withMT({
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,5 +9,3 @@ const config: Config = withMT({
   theme: {},
   plugins: [],
 });
-
-export default config;
