2a583f33afca43fc512750a19a9a93eb9d2235f9
Thomas Boehringer <dev@tboehringer.de>
2024-11-19 19:08:20 +0100

    update hero

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -10,17 +10,17 @@ function Hero() {
                 className="container mx-auto grid h-full gap-10 min-h-[60vh] w-full grid-cols-1 items-center lg:grid-cols-2">
                 <div className="row-start-2 lg:row-auto">
                     <Typography
+                        variant="lead"
+                        className="mb-4 text-3xl !text-gray-500 md:pr-16 xl:pr-28"
+                    >
+                        Herzlich Willkommen beim
+                    </Typography>
+                    <Typography
                         variant="h1"
                         color="blue-gray"
                         className="mb-4 lg:text-5xl !leading-tight text-3xl"
                     >
                         Sportverein Losaurach e.V.
-                    </Typography>
-                    <Typography
-                        variant="lead"
-                        className="mb-4 !text-gray-500 md:pr-16 xl:pr-28"
-                    >
-                        Schwarz-Gelbe Bomber!
                     </Typography>
                     <div className="grid">
                         <div className="mb-2 flex w-full flex-col gap-4 md:w-10/12 md:flex-row">
@@ -37,7 +37,7 @@ function Hero() {
                     width={1024}
                     height={1024}
                     alt="team work"
-                    src="/teams/herren-2022.jpeg"
+                    src="/image/50-jahre-svl.jpg"
                     className="h-[28rem] w-full rounded-xl object-cover"
                 />
             </div>
