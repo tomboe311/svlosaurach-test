75a769f1f14115ba5376b596503596b6bbf49174
Thomas Boehringer <dev@tboehringer.de>
2025-03-15 17:32:22 +0100

    update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,16 +5,18 @@ import {Button} from "@material-tailwind/react";
 
 export default function NotFound() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-yellow-400">
-            <h1 className="text-2xl font-bold">404 - Seite nicht gefunden</h1>
-            <p className="m-8 text-4xl text-center">Die angeforderte Seite konnte nicht gefunden werden. Es sieht so aus, als hätten wir das
-                Tor verfehlt!
-            </p>
-            <a href="/">
-                <Button color="yellow" className="mt-8">
-                Zurück zum Anstoßpunkt
-                </Button>
-            </a>
+        <div className="bg-[url('/images/404-not-found.jpg')] bg-cover xl:bg-center">
+            <div className="flex flex-col items-center justify-center min-h-screen text-yellow-400">
+                <h1 className="text-2xl font-bold">404 - Seite nicht gefunden</h1>
+                <p className="m-8 text-4xl text-center">Die angeforderte Seite konnte nicht gefunden werden. Es sieht so aus, als hätten wir das
+                    Tor verfehlt!
+                </p>
+                <a href="/">
+                    <Button color="yellow" className="mt-8">
+                        Zurück zum Anstoßpunkt
+                    </Button>
+                </a>
+            </div>
         </div>
     )
 }
